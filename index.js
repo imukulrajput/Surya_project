c27b7f2eb32df0f2fd0dfb01131cde6e576dcc54
Mukul Rajput <mukulrajput1430@gmail.com>
2026-03-08 00:04:16 +0530

    change limit again

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.use(cors({
 }));
 
 
-app.use(express.json({ limit: '25mb' }));
-app.use(express.urlencoded({ limit: '25mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
            
